b723db3ec12d447632d32b523c18aebc4109a552
PRESSO <caff1nepill@gmail.com>
2026-07-13 15:30:32 +0900

    chore(ios): add React Native Firebase Auth static linking

--- app.config.ts
+++ app.config.ts
@@ -180,6 +180,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
                     forceStaticLinking: [
                         'RNFBApp',
                         'RNFBAnalytics',
+                        'RNFBAuth',
                         'RNFBCrashlytics',
                         'RNFBMessaging',
                         'RNFBRemoteConfig',
